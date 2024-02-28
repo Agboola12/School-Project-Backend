daa8023185506c2a88ef60b164b9c78f5d121d8b
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-28 08:49:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require ("dotenv");
+const  mongoose  = require('mongoose');
+const PORT = process.env.PORT || 5001;
+const bodyParser = require('body-parser');
+const { rout } = require("./Router");
+const app = express();
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(cors({origin:"*"}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+dotenv.config();
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(process.env.URI).then(res =>{
+   console.log("database is connected");
+}).catch(err =>{
+   console.log( err.message );
+})
+
+
+
+
+app.get('/',(req, res)=>{
+   res.send("hello world")
+})
+
+app.use("/",rout )
+
+app.listen(PORT, ()=>{
+    console.log("My server is running on port "+ PORT)
+})
