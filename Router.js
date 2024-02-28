e9f542fde53d3290e24f0fd84e64ed9e990633a1
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-28 10:22:45 +0100

    wip on the login page

--- Router.js
+++ Router.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const { tutorRegister } = require('./controller/LecturerController');
 const rout = express.Router();
+
+rout.post("/tutorRegister", tutorRegister )
+
+
 
 
 module.exports = {rout}
--- controller/LecturerController.js
+++ controller/LecturerController.js
@@ -15,34 +15,23 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-
-
-
 // Registering User
-const userRegister = async (req, res) => {
-    // console.log(req.body);
+const tutorRegister = async (req, res) => {
+    
     const { fullName, email, department, password } = req.body;
     
-
     try {
-        
-
-        const alreadyExists = await userChurch.findOne({ email });
+        const alreadyExists = await schoolLearning.findOne({ email });
 
         if (alreadyExists) {
-            return res.status(200).json({
+            return res.status(203).json({
                 status: false,
                 message: "Email already exists",
             });
         }
-        const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '12h' });
         
-        const newUser = await schoolLearning.create({ joinDate, titles, fullName, email, mobile, postalCode, address, Dob, gender, code, department, admin, password, verificationToken  });
-
+        const newUser = await schoolLearning.create({ fullName, email, department, password  });
         
-        const verificationLink = `${process.env.FrontendUrl}verify?token=${verificationToken}`;
-        
-
         await transporter.sendMail({
             from: process.env.APP_MAIL,
             to: req.body.email,
@@ -50,27 +39,28 @@ const userRegister = async (req, res) => {
             html:
                 `<div>
                     <h1 class="text-center">RCCG Victory Centre</h1><br/>
-                    <p>Welcome to RCCG Victory Centre. Excited to journey together!</p>
+                    <p>Welcome to our Student Learning Site. Enjoy your stays with us</p>
                     <h3>${req.body.fullName}</h3>
-                    <p>Please click the following link to verify your account:</p>
-                    <a href="${verificationLink}">${verificationLink}</a>
                 </div>`
         })
             // console.log(newUser);
-        return res.status(200).json({
+        return res.status(201).json({
             status: true,
-            message: "Account Created! Welcome to RCCG Victory Centre. Excited to journey together!  Please check your email for verification.",
+            message: "Welcome to our Student Learning Site. Enjoy your stays with us.",
             data: newUser,
         })
 
     } catch (error) {
         console.error(error);
-        return res.status(200).json({
+        return res.status(203).json({
             status: false,
             message: "Oops, Onboarding failed",
         });
     }
 };
+
+
+module.exports = { tutorRegister}
 
 
   
